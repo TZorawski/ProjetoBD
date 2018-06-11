8c8c2376bd08881e8bb427e5b4831330063dcab5
Thais Zorawski <thaiszorawski@gmail.com>
2018-06-11 20:34:21 -0300

    Correção Modelo Relacional

--- GSA_UTFPR.sql
+++ GSA_UTFPR.sql
@@ -1,5 +1,5 @@
 -- MySQL Workbench Synchronization
--- Generated: 2018-06-10 00:07
+-- Generated: 2018-06-11 20:32
 -- Model: New Model
 -- Version: 1.0
 -- Project: Name of the project
@@ -9,147 +9,39 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `GSA_UTFPR` DEFAULT CHARACTER SET utf8 ;
+ALTER TABLE `gsa_utfpr`.`pessoas`
+ADD INDEX `fk_pessoas_cursos1_idx` (`cursos_sigla` ASC),
+DROP INDEX `fk_pessoas_cursos1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`cursos` (
-  `sigla` VARCHAR(5) NOT NULL,
-  `Nome` VARCHAR(500) NOT NULL,
-  PRIMARY KEY (`sigla`),
-  UNIQUE INDEX `sigla_UNIQUE` (`sigla` ASC),
-  UNIQUE INDEX `Nome_UNIQUE` (`Nome` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `gsa_utfpr`.`organizadores`
+ADD INDEX `fk_organizadores_pessoas1_idx` (`pessoas_cpf` ASC),
+DROP INDEX `fk_organizadores_pessoas1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`pessoas` (
-  `cpf` VARCHAR(11) NOT NULL,
-  `nome` VARCHAR(500) NOT NULL,
-  `email` VARCHAR(255) NOT NULL,
-  `resgistro` VARCHAR(10) NULL DEFAULT NULL,
-  `observacao` VARCHAR(500) NULL DEFAULT NULL,
-  `cursos_sigla` VARCHAR(5) NULL DEFAULT NULL,
-  PRIMARY KEY (`cpf`),
-  UNIQUE INDEX `cpf_UNIQUE` (`cpf` ASC),
-  INDEX `fk_pessoas_cursos1_idx` (`cursos_sigla` ASC),
-  CONSTRAINT `fk_pessoas_cursos1`
-    FOREIGN KEY (`cursos_sigla`)
-    REFERENCES `GSA_UTFPR`.`cursos` (`sigla`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `gsa_utfpr`.`ministrantes`
+ADD INDEX `fk_ministrantes_pessoas1_idx` (`pessoas_cpf` ASC),
+DROP INDEX `fk_ministrantes_pessoas1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`organizadores` (
-  `id` INT(11) NOT NULL,
-  `funcao` VARCHAR(500) NULL DEFAULT NULL,
-  `pessoas_cpf` VARCHAR(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_organizadores_pessoas1_idx` (`pessoas_cpf` ASC),
-  CONSTRAINT `fk_organizadores_pessoas1`
-    FOREIGN KEY (`pessoas_cpf`)
-    REFERENCES `GSA_UTFPR`.`pessoas` (`cpf`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `gsa_utfpr`.`eventos`
+ADD INDEX `fk_eventos_cursos1_idx` (`cursos_sigla` ASC),
+DROP INDEX `fk_eventos_cursos1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`ministrantes` (
-  `id` INT(11) NOT NULL,
-  `pessoas_cpf` VARCHAR(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
-  INDEX `fk_ministrantes_pessoas1_idx` (`pessoas_cpf` ASC),
-  CONSTRAINT `fk_ministrantes_pessoas1`
-    FOREIGN KEY (`pessoas_cpf`)
-    REFERENCES `GSA_UTFPR`.`pessoas` (`cpf`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `gsa_utfpr`.`atividades_has_ministrantes`
+ADD INDEX `fk_atividades_has_ministrantes_ministrantes1_idx` (`ministrantes_id` ASC),
+ADD INDEX `fk_atividades_has_ministrantes_atividades1_idx` (`atividades_id` ASC),
+ADD INDEX `fk_atividades_has_ministrantes_eventos1_idx` (`eventos_sigla` ASC),
+DROP INDEX `fk_atividades_has_ministrantes_eventos1_idx` ,
+DROP INDEX `fk_atividades_has_ministrantes_atividades1_idx` ,
+DROP INDEX `fk_atividades_has_ministrantes_ministrantes1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`eventos` (
-  `sigla` VARCHAR(20) NOT NULL,
-  `nome` VARCHAR(500) NOT NULL,
-  `data_inicio` DATE NOT NULL,
-  `data_fim` DATE NOT NULL,
-  `cursos_sigla` VARCHAR(5) NOT NULL,
-  PRIMARY KEY (`sigla`),
-  UNIQUE INDEX `nome_UNIQUE` (`nome` ASC),
-  UNIQUE INDEX `sigla_UNIQUE` (`sigla` ASC),
-  INDEX `fk_eventos_cursos1_idx` (`cursos_sigla` ASC),
-  CONSTRAINT `fk_eventos_cursos1`
-    FOREIGN KEY (`cursos_sigla`)
-    REFERENCES `GSA_UTFPR`.`cursos` (`sigla`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `gsa_utfpr`.`palestras`
+ADD INDEX `fk_palestras_atividades1_idx` (`atividades_id` ASC),
+DROP INDEX `fk_palestras_atividades1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`atividades` (
-  `id` INT(11) NOT NULL,
-  `nome` VARCHAR(255) NOT NULL,
-  `horas` FLOAT(11) NOT NULL,
-  `horario` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+ALTER TABLE `gsa_utfpr`.`minicursos`
+ADD INDEX `fk_minicursos_atividades1_idx` (`atividades_id` ASC),
+DROP INDEX `fk_minicursos_atividades1_idx` ;
 
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`atividades_has_ministrantes` (
-  `atividades_id` INT(11) NOT NULL,
-  `ministrantes_id` INT(11) NOT NULL,
-  `eventos_sigla` VARCHAR(20) NOT NULL,
-  PRIMARY KEY (`atividades_id`, `ministrantes_id`, `eventos_sigla`),
-  INDEX `fk_atividades_has_ministrantes_ministrantes1_idx` (`ministrantes_id` ASC),
-  INDEX `fk_atividades_has_ministrantes_atividades1_idx` (`atividades_id` ASC),
-  INDEX `fk_atividades_has_ministrantes_eventos1_idx` (`eventos_sigla` ASC),
-  CONSTRAINT `fk_atividades_has_ministrantes_atividades1`
-    FOREIGN KEY (`atividades_id`)
-    REFERENCES `GSA_UTFPR`.`atividades` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_atividades_has_ministrantes_ministrantes1`
-    FOREIGN KEY (`ministrantes_id`)
-    REFERENCES `GSA_UTFPR`.`ministrantes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_atividades_has_ministrantes_eventos1`
-    FOREIGN KEY (`eventos_sigla`)
-    REFERENCES `GSA_UTFPR`.`eventos` (`sigla`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`palestras` (
-  `titulo` VARCHAR(500) NOT NULL,
-  `id` INT(11) NOT NULL,
-  `atividades_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `atividades_id`),
-  INDEX `fk_palestras_atividades1_idx` (`atividades_id` ASC),
-  CONSTRAINT `fk_palestras_atividades1`
-    FOREIGN KEY (`atividades_id`)
-    REFERENCES `GSA_UTFPR`.`atividades` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`minicursos` (
-  `tema` VARCHAR(500) NOT NULL,
-  `id` INT(11) NOT NULL,
-  `atividades_id` INT(11) NOT NULL,
-  PRIMARY KEY (`id`, `atividades_id`),
-  INDEX `fk_minicursos_atividades1_idx` (`atividades_id` ASC),
-  CONSTRAINT `fk_minicursos_atividades1`
-    FOREIGN KEY (`atividades_id`)
-    REFERENCES `GSA_UTFPR`.`atividades` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`lista_presenca` (
+CREATE TABLE IF NOT EXISTS `gsa_utfpr`.`lista_presenca` (
   `atividades_id` INT(11) NOT NULL,
   `pessoas_cpf` VARCHAR(11) NOT NULL,
   PRIMARY KEY (`atividades_id`, `pessoas_cpf`),
@@ -157,16 +49,32 @@ CREATE TABLE IF NOT EXISTS `GSA_UTFPR`.`lista_presenca` (
   INDEX `fk_atividades_has_pessoas_atividades1_idx` (`atividades_id` ASC),
   CONSTRAINT `fk_atividades_has_pessoas_atividades1`
     FOREIGN KEY (`atividades_id`)
-    REFERENCES `GSA_UTFPR`.`atividades` (`id`)
+    REFERENCES `gsa_utfpr`.`atividades` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_atividades_has_pessoas_pessoas1`
     FOREIGN KEY (`pessoas_cpf`)
-    REFERENCES `GSA_UTFPR`.`pessoas` (`cpf`)
+    REFERENCES `gsa_utfpr`.`pessoas` (`cpf`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8;
+
+CREATE TABLE IF NOT EXISTS `gsa_utfpr`.`participante` (
+  `id` INT(11) NOT NULL,
+  `pessoas_cpf` VARCHAR(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC),
+  INDEX `fk_participante_pessoas1_idx` (`pessoas_cpf` ASC),
+  CONSTRAINT `fk_participante_pessoas1`
+    FOREIGN KEY (`pessoas_cpf`)
+    REFERENCES `gsa_utfpr`.`pessoas` (`cpf`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+DROP TABLE IF EXISTS `gsa_utfpr`.`lista_presenca` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
